ca5c8f211c36387eacc610c323a29789e0cb0fd1
JP Cimalando <jp-dev@inbox.ru>
2019-06-22 16:03:39 +0200

    remove use of boost ptree

--- sources/cws/cws80_program_textfmt.cpp
+++ sources/cws/cws80_program_textfmt.cpp
@@ -1,77 +1,51 @@
 #include "cws80_program.h"
+#include <SimpleIni.h>
 #include <fmt/format.h>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/info_parser.hpp>
-#include <algorithm>
-#include <sstream>
-#include <iostream>
 #include <stdexcept>
-namespace pt = boost::property_tree;
+#include <cstring>
 
 namespace cws80 {
 
-static pt::ptree ptree_from_text(const std::string &text)
-{
-    pt::ptree tree;
-    std::istringstream ins(text);
-    pt::read_info(ins, tree);
-    return tree;
-};
-
-static std::string ptree_to_text(const pt::ptree &tree)
-{
-    std::ostringstream outs;
-    pt::write_info(outs, tree);
-    return outs.str();
-}
-
 std::string Program::to_string() const
 {
-    pt::ptree tree;
+    CSimpleIniA ini;
     const Program &x = *this;
     char namebuf[8];
 
-    tree.put("SQ80\\File.type", "SQ80PROG");
-    tree.put("SQ80\\File.name", this->name(namebuf));
+    ini.SetValue("SQ80\\File", "type", "SQ80PROG");
+    ini.SetValue("SQ80\\File", "name", this->name(namebuf));
 
-#define PARAMETER_CASE(index, cat, name, gett, sett, min, max) \
-    {                                                          \
-        const char *key = "SQ80\\" #cat "." #name;             \
-        tree.put<i32>(key, (gett));                            \
-    }
-
+    #define PARAMETER_CASE(index, cat, name, gett, sett, min, max) \
+        ini.SetLongValue("SQ80\\" #cat, #name, (gett));
     EACH_PARAMETER(PARAMETER_CASE)
+    #undef PARAMETER_CASE
 
-#undef PARAMETER_CASE
+    std::string text;
+    if (ini.Save(text) != SI_OK)
+        throw std::logic_error("Cannot save SQ80 text program");
 
-    return ptree_to_text(tree);
+    return text;
 }
 
 Program Program::from_string(const std::string &text)
 {
     Program x;
-    pt::ptree tree = ptree_from_text(text);
 
-    const std::string type = tree.get<std::string>("SQ80\\File.type");
-    const std::string name = tree.get<std::string>("SQ80\\File.name");
+    CSimpleIniA ini;
+    bool load_ok = ini.LoadData(text) == SI_OK &&
+        strcmp(ini.GetValue("SQ80\\File", "type", ""), "SQ80PROG") == 0;
 
-    if (type != "SQ80PROG")
-        throw std::logic_error(
-            "Cannot load SQ80 text program: invalid file format");
+    if (!load_ok)
+        throw std::logic_error("Cannot load SQ80 text program");
 
+    const char *name = ini.GetValue("SQ80\\File", "name", "");
     memset(x.NAME, 0, 6);
-    memcpy(x.NAME, name.data(), std::min<size_t>(name.size(), 6));
+    memcpy(x.NAME, name, strnlen(name, 6));
 
-#define PARAMETER_CASE(index, cat, name, gett, sett, min, max) \
-    {                                                          \
-        const char *key = "SQ80\\" #cat "." #name;             \
-        i32 v = tree.get<i32>(key);                            \
-        (sett);                                                \
-    }
-
+    #define PARAMETER_CASE(index, cat, name, gett, sett, min, max) \
+        { i32 v = ini.GetLongValue("SQ80\\" #cat, #name); (sett); }
     EACH_PARAMETER(PARAMETER_CASE)
-
-#undef PARAMETER_CASE
+    #undef PARAMETER_CASE
 
     return x;
 }
